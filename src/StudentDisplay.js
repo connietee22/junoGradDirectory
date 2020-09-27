2787e238afe12a9c0d9da59df6aed219b740460b
connietee22 <connietsangphotos@gmail.com>
2020-09-27 17:43:34 -0400

    changed width of scroll to top button

--- src/StudentDisplay.js
+++ src/StudentDisplay.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-
-// i can change this to function?
 const StudentDisplay = (props) => {
 	return (
 		<>
@@ -38,45 +36,5 @@ const StudentDisplay = (props) => {
 		</>
 	);
 };
-
-// only need to access one student here so this does not need state!
-// class StudentDisplay extends Component {
-//     render() {
-//         return (
-// 			<>
-// 				<div>
-// 					<p className="studentName boldLabel">
-// 						{this.props.firstName} {this.props.lastName}
-// 					</p>
-// 					<p className="boldLabel">cohort {this.props.cohort}</p>
-
-// 					<p>
-// 						<a href={this.props.website}>
-// 							<i className="fas fa-desktop" aria-hidden="true"></i>
-// 							{this.props.website}
-// 						</a>
-// 					</p>
-
-// 					<p>
-// 						<a href={`https://github.com/${this.props.github}`}>
-// 							<i className="fab fa-github-square" aria-hidden="true"></i>
-// 							{this.props.github}
-// 						</a>
-// 					</p>
-// 					<p>
-// 						<a href={`https://www.linkedin.com/in/${this.props.linkedIn}`}>
-// 							<i className="fab fa-linkedin" aria-hidden="true"></i>
-// 							{this.props.linkedIn}
-// 						</a>
-// 					</p>
-// 					<p>
-// 						<span className="boldLabel">About Me: </span>
-// 						{this.props.funFact}
-// 					</p>
-// 				</div>
-// 			</>
-// 		);
-//     }
-// }
 
 export default StudentDisplay;
