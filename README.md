a6ee0bd64eb9df285b7e675b11813beeb6ed4bc3
connietee22 <connietsangphotos@gmail.com>
2020-09-30 22:28:57 -0400

    fixed tabbing - changed checkbox to button

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-Juno College Grad Directory: A directory of Juno College Bootcamp Grads, where users can input basic details, which will display in browsable user cards.
+Juno College Grad Directory: A directory of Juno College Bootcamp Grads, where users can input basic details, which will display in browsable user cards. Created by Connie Tsang at Juno College as part of the Immersive Web Development Bootcamp. Tools used: React, Firebase.
 
 // REMAINING STRETCH GOALS
-// Limiting 9 to a page
-// Hide the form on initial load and show two buttons in its place — one to browse, one for Juno grads
-Browse button will immediately scroll to directory
-Juno grad button will unveil the form for grads to add their name to the list
-// Uploading images
+// Adding pagination package
+// Uploading user images to firebase
--- src/components/AlphaButton.js
+++ src/components/AlphaButton.js
@@ -1,35 +1,15 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 
 /******BUTTON TO ALPHABETIZE CARDS*****/
 const AlphaButton = (props) => {
 
-        // using 
-	const [buttonText, setButtonText] = useState('sort by alphabet >>'); // setting default text for button;
-        const changeText = (text) => setButtonText(text); // function to set place new text
-        
-
 	return (
-		<form className="alphaContainer">
-			<label
-                        // on click, if !props.isAlpha (is true), then the list is alphabetized and the text in the label will change to sort by date, and vice versa
-				onClick={!props.isAlpha ? (() => changeText('Sort by date added >>')) : (() => changeText('Sort by alphabet >>'))}
-                                htmlFor='alphaButton'
-                                // if !props.isAlpha (is true) use the 'alphaLabel' class but the label/button. otherwise, use the darker 'alphaClicked' class.
-				className={(!props.isAlpha && 'alphaLabel') || (props.isAlpha && 'alphaClicked')}
-			>
-				{buttonText}{' '}
-				<input
-					type='checkbox'
-					id='alphaButton'
-					className='alphaButton'
-					tabIndex="1"
-					onChange={props.handleAlphaSubmit}
-				></input>
-			</label>
-		</form>
-	);
-};;
+		<div className="alphaContainer">
+			<button onClick={props.handleAlphaSubmit} className={props.isAlpha ? 'alphaClicked' : 'alphaLabel'}>{props.isAlpha ? 'Sort by date added >>' : 'Sort by alphabet >>'}</button>
+		</div>
+	)
+};
 
 export default AlphaButton;
